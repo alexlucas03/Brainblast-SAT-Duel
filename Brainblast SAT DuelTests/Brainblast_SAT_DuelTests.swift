798c60dcec61b216c007bff1dc988058630ef21f
alexlucas03 <alexlucas03@gmail.com>
2025-03-16 15:29:10 -0700

    Initial Commit

---
+++ Brainblast SAT DuelTests/Brainblast_SAT_DuelTests.swift
@@ -0,0 +1,17 @@
+//
+//  Brainblast_SAT_DuelTests.swift
+//  Brainblast SAT DuelTests
+//
+//  Created by Alex Lucas-Smith on 3/16/25.
+//
+
+import Testing
+@testable import Brainblast_SAT_Duel
+
+struct Brainblast_SAT_DuelTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
